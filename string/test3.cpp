dcea4159c1f58d9fa6d35a8df41a92c14d3eef04
zhaomengchao <1935812591@qq.com>
2019-11-14 17:48:05 +0800

    判断括号的合法性

---
+++ string/test3.cpp
@@ -0,0 +1,32 @@
+﻿//判断括号的合法性
+//例：合法括号只包括括号字符，并且一一对应
+//（（））a（）
+//不合法
+
+class Parenthesis {
+public:
+    bool chkParenthesis(string A, int n) {
+        
+        int count = 0;
+        for(int i = 0; i < n; i++)
+        {
+            if(A[i] == '(')
+            {
+                count++;
+            }
+            else if(A[i] == ')')
+            {
+                count--;
+            }
+            else
+            {
+                return false;
+            }
+            if(count < 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
