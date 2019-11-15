a92ea8e2cab2d4f22e6a852640e65fe6cae6f4a2
zhaomengchao <1935812591@qq.com>
2019-11-15 22:48:53 +0800

    将一段字符串中的前后单词交换，以单词为单位逆置

---
+++ string/test6.cpp
@@ -0,0 +1,15 @@
+﻿ //是将一段字符串中的前后单词交换，以单词为单位逆置。
+
+
+#include <iostream> 
+#include <string>
+ using namespace std; // cin读取string时自动会被空格分隔开，用另一个字符串存储进行逆序输出
+ int main()
+ {    
+    string s1, s2;
+    cin >> s2;
+    while (cin >> s1)
+        s2 = s1  + " " + s2;
+    cout << s2 << endl; 
+   return 0; 
+} 
