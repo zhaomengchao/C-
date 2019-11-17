8fd103a34cd4199bf345648b1bdac7be4fe4311a
zhaomengchao <1935812591@qq.com>
2019-11-17 19:00:16 +0800

    井字棋

---
+++ vector/test2.cpp
@@ -0,0 +1,58 @@
+﻿//井字棋
+//对角线/一行/一列全为相同数值时为true
+class Board {
+public:
+    bool checkWon(vector<vector<int> > board) {
+        int row = board.size();
+        int sum;
+        //判断行
+        for(int i = 0; i < row; ++i)
+        {
+            sum = 0;
+            for(int j = 0; j < row; ++j)
+            {
+                sum += board[i][j];
+            }
+            if(sum == row)
+            {
+                return true;
+            }
+        }
+        //判断列
+        for(int i = 0; i < row; ++i)
+        {
+            sum = 0;
+            for(int j = 0; j < row; ++j)
+            {
+                sum += board[j][i];
+            }
+            if(sum == row)
+            {
+                return true;
+            }
+        }
+        //判断正对角
+        sum = 0;
+        for(int i = 0; i < row;++i)
+        {
+            sum += board[i][i];
+        }
+        if(sum == row)
+        {
+            return true;
+        }
+        
+        //判断反对角
+        sum = 0;
+        for(int i = 0; i < row; ++i)
+        {
+            
+            sum += board[i][row - i - 1];
+        }
+        if( sum == row)
+        {
+            return true;
+        }
+        return false;
+    }
+};
