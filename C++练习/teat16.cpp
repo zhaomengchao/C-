59628c9640918377b8e1a3edfb56c816edb75abd
zhaomengchao <1935812591@qq.com>
2019-11-29 13:23:23 +0800

    字符串反转

---
+++ C++练习/teat16.cpp
@@ -0,0 +1,17 @@
+×Ö·û´®·´×ª
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    string s1;
+    cin >> s1;
+    for(int i = s1.size()-1; i >= 0;i--)
+    {
+         cout << s1[i];
+     }
+    
+    return 0;
+}
