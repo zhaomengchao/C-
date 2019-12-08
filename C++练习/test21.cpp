8ef423944e2f0169c8d1c3da6bd976d96f844621
zhaomengchao <1935812591@qq.com>
2019-12-08 21:24:02 +0800

    判断任意三条边是否构成三角形

---
+++ C++练习/test21.cpp
@@ -0,0 +1,24 @@
+﻿//三角形 判断任意三条边是否构成三角形
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    double a,b,c;
+    while(cin >> a >> b >> c)
+    {
+        if((a + b > c) && (a + c > b) && (c + b > a))
+        {
+            if(a - b < c && a - c < b && c - b < a)
+            {
+                cout << "Yes" << endl;
+            }
+            
+        }
+        else
+        {
+            cout << "No" << endl;
+        }
+    }
+    return 0;
+}
