778be99bd62a04256be2282d02f6ff16a6f20d27
zhaomengchao <1935812591@qq.com>
2019-12-08 21:13:45 +0800

    不用加减乘除求和

---
+++ C++练习/test20.cpp
@@ -0,0 +1,15 @@
+﻿//不用加减法求和
+
+class Solution {
+public:
+    int Add(int num1, int num2)
+    {
+        int c = num1 ^ num2;
+        int d = num1 & num2;
+        if(d!=0)
+        {
+            return Add(d << 1,c);
+        }
+        return c;
+    }
+};
