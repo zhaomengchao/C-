2a88d160d748678535db3ba88a4e7e104a6560f0
zhaomengchao <1935812591@qq.com>
2019-12-19 19:46:29 +0800

    二叉搜索树的范围和

---
+++ 基础练习代码/二叉搜索树的范围和.cpp
@@ -0,0 +1,42 @@
+//给定二叉搜索树的根结点 root，返回 L 和 R（含）之间的所有结点的值的和。
+//二叉搜索树保证具有唯一的值。
+//示例 1：
+//输入：root = [10,5,15,3,7,null,18], L = 7, R = 15
+//输出：32
+//示例?2：
+//输入：root = [10,5,15,3,7,13,18,1,null,6], L = 6, R = 10
+/输出：23
+
+
+//搜索二叉树左树的节点都比根节点的小
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int L, int R) {
+        if(root == nullptr)
+        {
+            return 0;
+        }
+        if(root->val < L)
+        {
+            return rangeSumBST(root->right,L,R);
+        }
+        if(root->val > R)
+        {
+            return rangeSumBST(root->left,L,R);
+        }
+
+        return root->val + rangeSumBST(root->left, L, R) + rangeSumBST(root->right, L, R); 
+        
+        
+        
+    }
+};
