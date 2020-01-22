b911323d5dc680027d0bd70368cd49d60e820f14
zhaomengchao <1935812591@qq.com>
2020-01-22 17:31:27 +0800

    最大子序列和《动态规划》

---
+++ 基础练习代码/最大子序列和.cpp
@@ -0,0 +1,25 @@
+//给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），
+//返回其最大和。
+
+
+class Solution
+{
+public:
+    int maxSubArray(vector<int> &nums)
+    {
+        //类似寻找最大最小值的题目，初始值一定要定义成理论上的最小最大值
+        int result = INT_MIN;
+        int numsSize = int(nums.size());
+        //dp[i]表示nums中以nums[i]结尾的最大子序和
+        vector<int> dp(numsSize);
+        dp[0] = nums[0];
+        result = dp[0];
+        for (int i = 1; i < numsSize; i++)
+        {
+            dp[i] = max(dp[i - 1] + nums[i], nums[i]);
+            result = max(result, dp[i]);
+        }
+
+        return result;
+    }
+};
