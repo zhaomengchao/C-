d220820a87385fe567170588368daf3e63841042
zhaomengchao <1935812591@qq.com>
2020-02-03 18:33:51 +0800

    成水最多的容器《双指针》

---
+++ 基础练习代码/盛水最多的容器.cpp
@@ -0,0 +1,34 @@
+﻿//盛水最多的容器
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int result = 0;
+        int heightSize = int(height.size());
+        int leftIndex = 0;
+        int rightIndex = heightSize - 1;
+
+        while (leftIndex != rightIndex)
+        {
+            int tmpHeight;
+            int tmpWidth = rightIndex - leftIndex;
+            //较短的一侧向中间移动
+            if (height[leftIndex] < height[rightIndex])
+            {
+                tmpHeight = height[leftIndex];
+                leftIndex++;
+            }
+            else
+            {
+                tmpHeight = height[rightIndex];
+                rightIndex--;
+            }
+            int tmpResult = tmpWidth * tmpHeight;
+            if (tmpResult > result)
+            {
+                result = tmpResult;
+            }
+        }
+        return result;
+    }
+
+};
