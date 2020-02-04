ef95691c864f8562df12183886680bf350e8e7e9
zhaomengchao <1935812591@qq.com>
2020-02-04 19:37:18 +0800

    跳跃游戏

---
+++ 基础练习代码/跳跃游戏.cpp
@@ -0,0 +1,16 @@
+﻿//定一个非负整数数组，你最初位于数组的第一个位置。
+//数组中的每个元素代表你在该位置可以跳跃的最大长度。
+//判断你是否能够到达最后一个位置。
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int k = 0;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(i > k)
+                return false;
+            k = max(k,i+nums[i]);
+        }
+        return true;
+    }
+};
