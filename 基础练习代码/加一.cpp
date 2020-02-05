4a8787b7c129c8ee491865980a1366294029250d
zhaomengchao <1935812591@qq.com>
2020-02-05 19:06:06 +0800

    加一

---
+++ 基础练习代码/加一.cpp
@@ -0,0 +1,23 @@
+class Solution {
+//给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+//最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+//你可以假设除了整数 0 之外，这个整数不会以零开头。
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for(int i = digits.size()-1; i >= 0; i--)
+        {
+            if(digits[i] == 9)
+            {
+                digits[i] = 0;
+            }
+            else
+            {
+                digits[i]++;
+                return digits;
+            }
+        }
+        digits.push_back(0);
+        digits[0] = 1;
+        return digits;
+    }
+};
