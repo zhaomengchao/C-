86233c7961fd93d91af9ba7aabf651cd3cde2667
zhaomengchao <1935812591@qq.com>
2020-02-10 16:49:14 +0800

    基础练习

---
+++ 基础练习代码/大写字母转小写.cpp
@@ -0,0 +1,14 @@
+﻿lass Solution {
+public:
+    string toLowerCase(string str) {
+        int len = str.size();
+        for(int i = 0; i < len ;i++)
+        {
+            if(str[i] >= 'A' && str[i] <= 'Z')
+            {
+                str[i] += 32;
+            }
+        }
+        return str;
+    }
+};
