c46a57b5fa8c725d9f72257cf2622ad83045b323
zhaomengchao <1935812591@qq.com>
2020-02-18 16:11:06 +0800

    单词识别map

---
+++ 基础练习代码/单词识别.cpp
@@ -0,0 +1,33 @@
+﻿//A blockhouse is a small castle that has four openings through which to shoot.
+输出
+
+//a:2
+//blockhouse:1 castle:1 four:1 has:1 is:1 openings:1 shoot:1 small:1 that:1 through:1 to:1 which:1
+#include <iostream>
+#include <map>
+ 
+using namespace std;
+ 
+int main()
+{
+    map<string,int>dict;
+    string str;
+    while(cin>>str)
+    {
+        for(int i = 0;i < str.size();i++)
+        {
+            if(str[i] >= 'A' && str[i] <= 'Z')
+                str[i] += 32;
+            if(str[i] == ',')
+                str.erase(i,1);
+            if(str[i] == '.')
+                str.erase(i,1);
+        }
+        dict[str]++;//统计相同单词出现的次数
+    }
+    for(auto e: dict)
+    {
+        cout<<e.first<<":"<<e.second<<endl;
+    }
+    return 0;
+}
