81dd873f514745e8e0b8c6a2f65c556172146003
zhaomengchao <1935812591@qq.com>
2020-02-29 18:48:47 +0800

    合并连个有序列表

---
+++ 基础练习代码/合并两个有序列表.cpp
@@ -0,0 +1,43 @@
+﻿//合并两个有序列表
+//输入：1->2->4, 1->3->4
+//输出：1->1->2->3->4->4
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* ret = new ListNode(0);
+        ListNode* head = ret;
+
+        while(l1 && l2)
+        {
+            if(l1->val <= l2->val)
+            {
+                head->next = l1;
+                l1 = l1->next;
+            }
+            else
+            {
+                head->next = l2;
+                l2 = l2->next;
+            }
+            head = head->next;
+        }
+
+        if(l1 != NULL)
+        {
+            head->next = l1;
+        }
+        else
+        {
+            head->next = l2;
+        }
+        return ret->next;
+    }
+};
