478b4fea1ab84008b05c1c5c3f2217f5eae5466b
zhaomengchao <1935812591@qq.com>
2020-03-01 18:03:22 +0800

    从尾到头打印链表(栈，数组实现)

---
+++ 基础练习代码/从尾到头打印链表.cpp
@@ -0,0 +1,27 @@
+﻿//输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> reversePrint(ListNode* head) {
+        vector<int> tmp;
+        stack<int> s;
+        while(head != NULL)
+        {
+            s.push(head->val);
+            head = head->next;
+        }
+        while(!s.empty())
+        {
+            tmp.push_back(s.top());
+            s.pop();
+        }
+        return tmp;
+    }
+};
