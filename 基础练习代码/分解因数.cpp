f6f788cfccd695c0e3abbfb0aed8ace039e13e34
zhaomengchao <1935812591@qq.com>
2020-03-02 14:51:09 +0800

    分解因数

---
+++ 基础练习代码/分解因数.cpp
@@ -0,0 +1,36 @@
+﻿//分解因数
+//分解因数，分解成 n = 1 < a1* <=a2*<= *a3
+
+//10 = 2 * 5；
+//18 = 2 * 3 * 3
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	while (cin >> n)
+	{
+		cout << n << " = ";
+		for (int i = 2; i <= n; i++)
+		{
+			while (n % i == 0)
+			{
+				if (n != i)
+				{
+					cout << i << " * ";
+				}
+				else
+				{
+					cout << n;
+				}
+				n = n / i;
+			}
+			
+		}
+
+	}
+	return 0;
+}
