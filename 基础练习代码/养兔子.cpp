df4a240c58cead09bc2b3be902b6fa8850ef352e
zhaomengchao <1935812591@qq.com>
2020-03-05 15:41:41 +0800

    养兔子

---
+++ 基础练习代码/养兔子.cpp
@@ -0,0 +1,38 @@
+﻿//一只成熟的兔子每天能产下一胎兔子，每只小兔子的成熟期是一天，某人领养一只小兔子，
+//请问n天后,还会有多少只小兔子
+
+
+
+// write your code here cpp
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+    int n;
+    while(cin >> n)
+    {
+        if(n <= 3)
+        {
+            cout << n << endl;
+        }
+        else
+        {
+            long a = 2;
+            long b = 3;
+            long c;
+            
+            for(int i = 4; i <= n; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            cout << c << endl;
+        }
+    }
+    
+    return 0;
+    
+}
