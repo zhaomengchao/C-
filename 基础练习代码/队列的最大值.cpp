1a3df8c1a0ad87e0f652a0bb54359a7a2b24e25e
zhaomengchao <1935812591@qq.com>
2020-03-24 14:50:10 +0800

    队列的最大值

---
+++ 基础练习代码/队列的最大值.cpp
@@ -0,0 +1,32 @@
+﻿class MaxQueue {
+    queue<int> q;
+    deque<int> p;
+public:
+    MaxQueue() {
+    }
+    
+    int max_value() {
+        if (p.empty())
+            return -1;
+        return p.front();
+    }
+    
+    void push_back(int value) {
+        while (!p.empty() && p.back() < value) {
+            p.pop_back();
+        }
+        p.push_back(value);
+        q.push(value);
+    }
+    
+    int pop_front() {
+        if (q.empty())
+            return -1;
+        int ans = q.front();
+        if (ans == p.front()) {
+            p.pop_front();
+        }
+        q.pop();
+        return ans;
+    }
+};
