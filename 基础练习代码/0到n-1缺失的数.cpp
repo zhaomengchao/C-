2077b3d50c07d07ca82602ab3bb05f306aaafd50
zhaomengchao <1935812591@qq.com>
2020-03-25 15:07:14 +0800

    0～n-1中缺失的数字（二分法）

---
+++ 基础练习代码/0到n-1缺失的数.cpp
@@ -0,0 +1,36 @@
+﻿/*一个长度为n-1的递增排序数组中的所有数字都是唯一的，
+并且每个数字都在范围0～n-1之内。在范围0～n-1内的n个数字中
+有且只有一个数字不在该数组中，请找出这个数字。*/
+/*输入: [0,1,3]
+输出: 2
+二分法
+*/
+
+
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        if(nums.empty())
+        {
+            return 0;
+        }
+        int len = nums.size();
+        int left = 0;
+        int right = len - 1;
+
+        while(left <= right)
+        {
+            int mid = (left + right) / 2;
+            if(nums[mid] == mid)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+};
