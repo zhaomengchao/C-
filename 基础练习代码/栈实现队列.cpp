bf5dd853c6cc23bd00ae96df842933cabe946bd3
zhaomengchao <1935812591@qq.com>
2020-03-26 14:30:33 +0800

    栈实现队列

---
+++ 基础练习代码/栈实现队列.cpp
@@ -0,0 +1,41 @@
+﻿//栈实现队列
+class CQueue {
+    stack<int> s1;
+    stack<int> s2;
+public:
+    CQueue() {
+
+    }
+    
+    void appendTail(int value) {
+        s1.push(value);
+    }
+    
+    int deleteHead() {
+        if(s2.empty())
+        {
+            if(s1.empty())
+            {
+                return -1;
+            }
+            else
+            {
+                while(!s1.empty())
+                {
+                    s2.push(s1.top());
+                    s1.pop();
+                }
+            }
+        }
+        int head = s2.top();
+        s2.pop();
+        return head;
+    }
+};
+
+/**
+ * Your CQueue object will be instantiated and called as such:
+ * CQueue* obj = new CQueue();
+ * obj->appendTail(value);
+ * int param_2 = obj->deleteHead();
+ */
