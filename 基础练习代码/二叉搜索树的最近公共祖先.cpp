e89280119f71c630de348bc581f93c79337abb23
zhaomengchao <1935812591@qq.com>
2020-04-13 15:30:27 +0800

    二叉搜索树的最近公共祖先，递归实现

---
+++ 基础练习代码/二叉搜索树的最近公共祖先.cpp
@@ -0,0 +1,29 @@
+// 二叉搜索树的最近公共祖先
+// 二叉搜索树的最近公共祖先
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if((root -> val - p->val)*(root -> val - q ->val) <= 0 ){
+            return root;
+        }
+
+        if(root->val > p->val)
+        {
+            return lowestCommonAncestor(root->left,p,q);
+        }
+        if(root->val < q->val)
+        {
+            return lowestCommonAncestor(root->right,p,q);
+        }
+        return NULL;
+    }
+};
