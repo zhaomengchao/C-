09f70b5f0dec846ddc97b94d99c5bdf0443efb9e
zhaomengchao <1935812591@qq.com>
2020-04-21 15:36:28 +0800

    单值二叉树（递归）

---
+++ 基础练习代码/单值二叉树.cpp
@@ -0,0 +1,29 @@
+﻿//单值二叉树--所有结点值相等
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isUnivalTree(TreeNode* root) {
+        return _isUnivalTree(root, root->val);
+    }
+    bool _isUnivalTree(TreeNode* root, int val)
+    {
+        if(root == NULL)
+        {
+            return true;
+        }
+        if(root->val != val)
+        {
+            return false;
+        }
+        return _isUnivalTree(root->left,root->val)&&_isUnivalTree(root->right,root->val);
+    }
+       
+};
