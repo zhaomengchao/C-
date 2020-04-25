84238457f1b6b158b43df62005a2ef58ae295d2e
zhaomengchao <1935812591@qq.com>
2020-04-25 14:33:34 +0800

    重建二叉树

---
+++ 基础练习代码/重建二叉树.cpp
@@ -0,0 +1,29 @@
+﻿//重建二叉树，给出前序和中序构建
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        //递归分治
+        return recursionBuild(preorder.begin(),preorder.end(),inorder.begin(),inorder.end());
+    }
+
+    //递归分治
+    TreeNode* recursionBuild(vector<int>::iterator preBegin, vector<int>::iterator preEnd,vector<int>::iterator inBegin, vector<int>::iterator inEnd )
+    {
+        if(inEnd==inBegin) return NULL;
+        TreeNode* cur = new TreeNode(*preBegin);
+        auto root = find(inBegin,inEnd,*preBegin);
+        cur->left = recursionBuild(preBegin+1,preBegin+1+(root-inBegin),inBegin,root);
+        cur->right = recursionBuild(preBegin+1+(root-inBegin),preEnd,root+1,inEnd);
+        return cur;
+    }
+};
+
