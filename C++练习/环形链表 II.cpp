4d2f05e883b87bd372e7f290b61a0964b78d64bd
zhaomengchao <1935812591@qq.com>
2020-05-12 15:10:23 +0800

    环形链表双指针

---
+++ C++练习/环形链表 II.cpp
@@ -0,0 +1,35 @@
+//»·ĞÎÁ´±í
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+
+        while(fast != NULL && fast->next != NULL)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+
+            if(fast == slow)
+            {
+                fast = head;
+                while(fast != slow)
+                {
+                    fast = fast->next;
+                    slow = slow->next;
+                }
+                return fast;
+                break;
+            }
+        }
+    return nullptr;
+    }
+};
