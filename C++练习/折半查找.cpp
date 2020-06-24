c2e2dfeb0271f9049f0c388a65709b25e16c7a83
zhaomengchao <1935812591@qq.com>
2020-06-24 18:21:08 +0800

    二分查找

---
+++ C++练习/折半查找.cpp
@@ -0,0 +1,22 @@
+//’€∞Î≤È’“µƒ∑«µ›πÈ
+class Solution 
+{
+public:
+    int search(vector<int>& nums, int target) 
+    {
+        int low=0,high=nums.size()-1;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+
+            if(target==nums[mid]) 
+                return mid;
+            if(target<nums[mid]) 
+                high=mid-1;
+            else 
+                low=mid+1;
+        }
+        return -1;
+    }
+
+};
