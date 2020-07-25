852c8b8ea24c085fc6d5fe9036e9de5f9f70ffe6
zhaomengchao <1935812591@qq.com>
2020-07-25 18:02:42 +0800

    两个数组的交集 II

---
+++ C++练习/两个数组的交集 II (2).cpp
@@ -0,0 +1,27 @@
+/*
+https://leetcode-cn.com/problems/intersection-of-two-arrays-ii/
+两个数组的交集 II
+*/
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        if (nums1.size() > nums2.size()) {
+            return intersect(nums2, nums1);
+        }
+        unordered_map <int, int> m;
+        for (int num : nums1) {
+            ++m[num];
+        }
+        vector<int> intersection;
+        for (int num : nums2) {
+            if (m.count(num)) {
+                intersection.push_back(num);
+                --m[num];
+                if (m[num] == 0) {
+                    m.erase(num);
+                }
+            }
+        }
+        return intersection;
+    }
+};
